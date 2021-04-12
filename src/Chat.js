d21d90d95d28bb48190322dc82c62a27b3192a17
Thoshitha Kaduruwewa <thoshithakaduru.dev@gmail.com>
2021-04-12 19:57:33 +0530

    added barebone chat component

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,17 @@
 import React from "react";
-import "./Chat.css"
+import "./Chat.css";
 
 function ChatVeiw() {
   return (
     <div className="chat">
-      <h1>Chat</h1>
+      <div className="chat__container">
+        <div className="chat__header">header</div>
+        <div className="chat__body">body</div>
+        <div className="chat__footer">
+          <input className="chat" type="text" placeholder="Say something..." />
+          <button className="send">send message</button>
+        </div>
+      </div>
     </div>
   );
 }
