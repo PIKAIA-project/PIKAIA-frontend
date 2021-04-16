703b50360c73665c5d2fa523fe6579d8ec2e64e9
Shuhaib-Ahamed <shuhaib.2019678@iit.ac.lk>
2021-04-16 16:41:12 +0530

    Charts blur

--- src/Chart.js
+++ src/Chart.js
@@ -3,18 +3,21 @@ import "./Chart.css";
 
 function Chart() {
   return (
-    <div className="chart">
-      <div className="chart__header">
-        <h1>Analytics</h1>
+    <>
+      <div className="chart">
+        <div className="chart__header">
+          <h1>Emotion Analytics</h1>
+        </div>
+        <div className="chart__container">
+          <div className="chart__one"></div>
+          <div className="chart__two"></div>
+          <div className="chart__three"></div>
+          <div className="chart__four"></div>
+          <div className="chart__five"></div>
+          <div className="chart__six"></div>
+        </div>
       </div>
-      <div className="chart__container">
-        <div className="chart__one"></div>
-        <div className="chart__two"></div>
-        <div className="chart__three"></div>
-        <div className="chart__four"></div>
-        <div className="chart__five"></div>
-      </div>
-    </div>
+    </>
   );
 }
 
