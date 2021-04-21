20c432bb86d4c25ec0ac97cb4db4c8dafc082358
shehan2001 <shehansaverimuttu@gmail.com>
2021-04-21 10:38:55 +0530

    About page

--- src/About.js
+++ src/About.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import './About.css'
+import Bot1 from '../src/Images/Bot.png';
+import Comp from '../src/Images/HaxMas.jpg';
+import CompHACK from '../src/Images/IIT HACK.jpeg';
+import Complement from '../src/Images/Complements.jpg';
 
 const About = () => {
   const [toggleState, setToggleState] = useState(1)
@@ -22,7 +26,7 @@ const About = () => {
             }
             onClick={() => toggleTab(1)}
           >
-            About Project PIKAIA
+          <div class="tab"> About Project PIKAIA</div>
           </button>
           <button
             className={
@@ -32,7 +36,8 @@ const About = () => {
             }
             onClick={() => toggleTab(2)}
           >
-            Meet the team
+          
+          <div class="tab">  Meet the team </div>
           </button>
           <button
             className={
@@ -42,7 +47,7 @@ const About = () => {
             }
             onClick={() => toggleTab(3)}
           >
-            Mental Health Awareness
+            <div class="tab"> Mental Health Awareness </div>
           </button>
         </div>
 
@@ -84,7 +89,45 @@ const About = () => {
 
 // TODO: add tab 1 content here
 const Tab1 = () => {
-  return <h2>About our project code comes here</h2>
+  return <div class="About_main">
+  <div class="about_card">
+    
+      <img src={Bot1}  alt="Pikaia" class="about_image"/>
+      <h1 class="H1_header">PROJECT PIKAIA </h1>
+      <p class="P1_Para" >~ Our vision and our mission ~</p>
+
+      <hr class="hr1"></hr>
+      
+      <p class="P2_Para">Lorem ipsum dolor sit amet, consectetur  adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      
+      <hr class="hr2"></hr>
+      
+      <h1 class="about_comp">Competitions Team PhyOps has joined</h1>
+      <div class="container_1">
+      <img src={Comp}  alt="HaxMas" class="about_img_haxmas"/>
+      <p class="About_img_comp_para">Team PyOps, Project PIKAIA earning 4th place out of 40 impressive and outstanding ideas. HaxMas was also their very first team event</p>
+      </div>
+
+      <hr class="hr2"></hr>
+
+      <div class="container_1">
+      <p class="About_img_compHack_para">Currently participating in IIT HACK with the goal of trying to get to the top 3 and gaining experience for self and team growth</p>
+      <img src={CompHACK}  alt="IIT HACK" class="about_img_IITHACK"/>
+     
+      </div>
+
+      <hr class="hr2"></hr>
+
+      <div class="container_1">
+      <img src={Complement}  alt="Ayeee!" class="about_img_Complements"/>
+      <p class="About_img_Complement_para"> Mrs Anna Kalm (Founder CEO of Ascentic) and Mrs Dhanisha Paul (Director Delivery at Sysco Labs) acknowledge team Pyop's project PIKAIA both in Linkedin and on the day of HaxMas 2021 </p>
+      </div>
+      </div>
+      
+
+  
+
+</div>
 }
 
 // TODO: add tab 2 content here
@@ -314,7 +357,21 @@ const Tab2 = () => {
 
 // TODO: add tab 3 content here
 const Tab3 = () => {
-  return <h2>Mental Awareness Code comes here</h2>
+  return <div class="About_main">
+  <div class="about_card">
+    
+      <img src={Bot1}  alt="Pikaia" class="about_image"/>
+      <h1 class="H1_header">PROJECT PIKAIA </h1>
+      <p class="P1_Para" >~ Mental health? ~</p>
+
+      <hr></hr>
+      
+      <p class="P2_Para">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+  
+
+  
+
+</div>
 }
 
 export default About
